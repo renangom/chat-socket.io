d9b4a410c5aa8a778cedcacc410591a6e59e91fe
Renan <renan23eduardo@gmail.com>
2022-11-13 23:52:21 -0300

    Create pages in client side and configure styled-components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { Global } from './GlobalCss'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Global />
   </React.StrictMode>
 )
